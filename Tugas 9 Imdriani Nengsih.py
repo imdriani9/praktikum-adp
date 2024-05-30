dd70b66315c91db855a4020071340cf1b02a3187
imdriani9 <163278923+imdriani9@users.noreply.github.com>
2024-05-29 19:20:30 -1200

    Add files via upload

---
+++ Tugas 9 Imdriani Nengsih.py
@@ -0,0 +1,34 @@
+import os
+from termcolor import cprint
+
+os.system('cls')
+print("Nama   : Imdriani Nengsih")
+print("No. BP : 2310431035")
+print("shift  : 4")
+
+print()
+print()
+for i in range (1):
+    cprint("   "*14, "white", "on_light_cyan")
+for i in range (1):
+    cprint("︶"*21 , "white", "on_light_cyan")
+for i in range (1):
+    cprint(" ` "*14, "white", "on_light_cyan")
+for i in range (1):
+    cprint("   "*14, "black", "on_light_cyan")
+for i in range (1):
+    cprint("   "*14, "black", "on_light_cyan")
+for i in range (1):    
+    cprint("๑ H  A  P  P  Y   B  I  R  T  H  D  A  Y ๑", "yellow", "on_light_cyan",)
+for i in range(1):
+    cprint(" ✧ "*14, "white", "on_light_cyan")
+for i in range (1):
+    cprint("   "*14, "black", "on_light_cyan")
+for i in range (1):
+    cprint("   "*14, "black", "on_light_cyan")    
+for i in range (1):
+    cprint("   "*14, "black", "on_light_cyan")
+for i in range (1):
+    cprint(" ✿ "*14, "white", "on_light_cyan")
+for i in range (1):
+    cprint(" ෴ "*14, "white", "on_light_cyan")
